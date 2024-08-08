17e452379ac9f9abf92726c8526a5ca945d421ec
nenahui <kanatraccoon@gmail.com>
2024-08-09 01:15:49 +0600

    Prepared the appearance for further work

--- cipher-front/src/App.tsx
+++ cipher-front/src/App.tsx
@@ -1,11 +1,21 @@
-import { Typography } from 'antd';
+import { DownOutlined, UpOutlined } from '@ant-design/icons';
+import { Button, Flex, Input } from 'antd';
+import TextArea from 'antd/es/input/TextArea';
 import React from 'react';
 import { Container } from './components/Container/Container';
 
 export const App: React.FC = () => {
   return (
     <Container>
-      <Typography.Text>Initial App</Typography.Text>
+      <TextArea rows={5} placeholder={'Decoded message…'} />
+      <Flex justify={'space-between'} gap={'small'} style={{ width: '100%' }}>
+        <Input placeholder={'Password…'} />
+        <Flex gap={'small'}>
+          <Button type={'text'} icon={<UpOutlined />} />
+          <Button type={'text'} icon={<DownOutlined />} />
+        </Flex>
+      </Flex>
+      <TextArea rows={5} placeholder={'Encoded message…'} />
     </Container>
   );
 };
--- cipher-front/src/main.tsx
+++ cipher-front/src/main.tsx
@@ -1,10 +1,15 @@
+import { ConfigProvider, theme } from 'antd';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>
+  <StrictMode>
+    <ConfigProvider
+      theme={{ algorithm: [theme.darkAlgorithm, theme.compactAlgorithm] }}
+    >
+      <App />
+    </ConfigProvider>
+  </StrictMode>
 );
